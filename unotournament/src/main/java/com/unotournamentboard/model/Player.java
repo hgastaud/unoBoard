8bbff53f1f1a53fc957f61ba6b5bddb0f5b43397
Hernan Gastaud <hgastaud@gmail.com>
2012-10-15 17:57:44 -0300

    Added Member relationship.

--- unotournament/src/main/java/com/unotournamentboard/model/Player.java
+++ unotournament/src/main/java/com/unotournamentboard/model/Player.java
@@ -6,6 +6,7 @@ import org.neo4j.graphdb.Direction;
 import org.springframework.data.neo4j.annotation.GraphId;
 import org.springframework.data.neo4j.annotation.NodeEntity;
 import org.springframework.data.neo4j.annotation.RelatedTo;
+import org.springframework.data.neo4j.annotation.RelatedToVia;
 
 /**
  * Represents a single player of an UNO Tournament.
@@ -16,76 +17,77 @@ import org.springframework.data.neo4j.annotation.RelatedTo;
 @NodeEntity
 public class Player {
 
-	@GraphId
-	private long id;
+    @GraphId
+    private long id;
 
-	private String name;
+    private String name;
 
-	private String lastName;
+    private String lastName;
 
-	private String username;
+    private String username;
 
-	private String password;
+    private String password;
 
-	private String country;
+    private String country;
 
-	@RelatedTo(type = "FRIEND", direction = Direction.OUTGOING)
-	private Set<Player> friends;
+    @RelatedTo(type = "FRIEND", direction = Direction.OUTGOING)
+    private Set<Player> friends;
 
-	public Set<Player> getFriends() {
-		return friends;
-	}
+    @RelatedToVia(type = "MEMBER_OF", direction = Direction.OUTGOING)
+    public Set<Player> getFriends() {
+        return friends;
+    }
 
-	public void setFriends(Set<Player> friends) {
-		this.friends = friends;
-	}
+    public void setFriends(Set<Player> friends) {
+        this.friends = friends;
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getCountry() {
-		return country;
-	}
+    public String getCountry() {
+        return country;
+    }
 
-	public void setCountry(String country) {
-		this.country = country;
-	}
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
 }
